baedd714f99b882569c8e0bdace9dc86d62fbd07
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2023-08-14 14:55:11 +0530

    heya

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
+import { FaDownload, FaBars, FaTimes } from 'react-icons/fa'; // Import the download, bars (hamburger), and times (close) icons
 import './Navbar.css';
-import signatureLogo from '../assets/images/signature.png'
+import signatureLogo from '../assets/images/signature.png';
+
 function Navbar() {
+  const [menuActive, setMenuActive] = useState(false); // State to manage mobile menu
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-content">
-      <img src={signatureLogo} alt="Signature Logo" className="navbar-logo" />
-        <a href="#home">Home</a>
-        <a href="#skills">Skills</a>
-        <a href="#projects">
-        <Link to="projects" smooth={true} duration={1000}>
-          Projects
-        </Link></a>
-        <a href="#contact">Contact</a>
+        <img src={signatureLogo} alt="Signature Logo" className="navbar-logo" />
+        
+        {/* Menu Toggle Button */}
+        <div className="menu-toggle" onClick={toggleMenu}>
+          {menuActive ? <FaTimes /> : <FaBars />}
+        </div>
+
+        <div className={`nav-links ${menuActive ? 'active' : ''}`}>
+          <a href="#home">Home</a>
+          <a href="#skills">Skills</a>
+          <a href="#projects">
+            <Link to="projects" smooth={true} duration={1000}>
+              Projects
+            </Link>
+          </a>
+          <a href="#contact">Contact</a>
+          <a href="/path-to-your-resume.pdf" download="your-resume.pdf">
+            Resume <FaDownload />
+          </a>
+        </div>
       </div>
     </nav>
   );
