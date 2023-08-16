8588678ce2178c55c2b116f2fd9fc01f924c8d1c
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2023-08-16 12:45:19 +0530

    editing....🧊

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React from 'react';
 import { Element } from 'react-scroll'; 
 import Navbar from './components/Navbar';
@@ -6,19 +7,15 @@ import Technologies from './components/Technologies';
 import ProjectCaraousel from './components/ProjectCaraousel';
 import Education from './components/Education';
 
-// ... other imports
-
 function App() {
   return (
     <div className="App">
       <Navbar />
       <ProfileCard />
       <Technologies />
-    {/* Use Element to define the section for Project.js */}
       <Element name="projects" className="element">
         <ProjectCaraousel />
       </Element>
-      {/* ... other components */}
       <Education />
     </div>
   );
