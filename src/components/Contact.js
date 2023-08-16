da0b71e3e8607e50985bb4a5df1fa88113be2a97
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2023-08-16 23:58:11 +0530

    editing ....🧊🧊

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,66 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './Contact.css';
 import contactImg from '../assets/images/contact.png'
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
+    const form = useRef();
+    const [isSent, setIsSent] = useState(false);
+    const [userName, setUserName] = useState(''); // State for name
+    const [userEmail, setUserEmail] = useState(''); // State for email
+    const [message, setMessage] = useState(''); // State for message
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_1kqc8pj', 'template_f22tfxk', form.current, 'jbOmNP17EODgz96Im')
+            .then((result) => {
+                console.log(result.text);
+                setIsSent(true);
+                // Clear the state after email is sent
+                setUserName('');
+                setUserEmail('');
+                setMessage('');
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <div className="contact-container">
-             <h2 className="education-heading">Get in touch !</h2>
+            <h2 className="education-heading">Get in touch !</h2>
             <div className="contact-image">
                 <img src={contactImg} alt="contact-us" />
             </div>
             <div className="contact-form">
                 <h2>Contact Me</h2>
-                <form>
-                    <input type="text" placeholder="Name" required />
-                    <input type="email" placeholder="Email" required />
-                    <textarea placeholder="Message" required></textarea>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input 
+                        type="text" 
+                        placeholder="Name" 
+                        name="user_name" 
+                        value={userName} 
+                        onChange={e => setUserName(e.target.value)} 
+                        required 
+                    />
+                    <input 
+                        type="email" 
+                        placeholder="Email" 
+                        name="user_email" 
+                        value={userEmail} 
+                        onChange={e => setUserEmail(e.target.value)} 
+                        required 
+                    />
+                    <textarea 
+                        placeholder="Message" 
+                        name="message" 
+                        value={message} 
+                        onChange={e => setMessage(e.target.value)} 
+                        required 
+                    />
                     <button type="submit">Send</button>
                 </form>
+                {isSent && <div className="sent-message">Sent ✔️</div>}
             </div>
         </div>
     );
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -6,14 +6,29 @@ import jsIcon from '../assets/icons/js.png';
 import atomIcon from '../assets/icons/atom.png';
 import nodeJSIcon from '../assets/icons/nodejs.png';
 import expressJSIcon from '../assets/icons/expressjs.png';
+import nginxIcon from '../assets/icons/nginx.png'
+import npmIcon from '../assets/icons/outline.png'
+import BootstrapIcon from '../assets/icons/bootstrap.png'
+import TailwindIcon from '../assets/icons/tailwind.png'
+import GitIcon from '../assets/icons/git.png'
+import LinuxIcon from '../assets/icons/linux.png'
+import DockerIcon from '../assets/icons/docker.png'
+import mongodbIcon from '../assets/icons/mongodb.png'
 const technologies = [
   { name: 'HTML', icon: html5Icon },
   { name: 'CSS', icon: css3Icon },
   { name: 'JavaScript', icon: jsIcon },
   { name: 'React', icon: atomIcon },
+  { name: 'MongoDB', icon: mongodbIcon },
   { name: 'Node.js', icon: nodeJSIcon },
   { name: 'Express.js', icon: expressJSIcon },
-  // Add more technologies as needed
+  { name: 'Nginx', icon: nginxIcon },
+  { name: 'NPM', icon: npmIcon },
+  { name: 'Bootstrap', icon: BootstrapIcon },
+  { name: 'Tailwind', icon: TailwindIcon },
+  { name: 'Git', icon: GitIcon },
+  { name: 'Docker', icon: DockerIcon },
+  { name: 'Linux', icon: LinuxIcon },
 ];
 
 const Technologies = () => {
